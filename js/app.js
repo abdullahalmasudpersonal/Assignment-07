0bb12d414510b21503d4689998b2748d506adc9d
masud <abdullahalmasud01737906772@gmail.com>
2022-03-13 00:52:54 +0600

    third commit

--- js/app.js
+++ js/app.js
@@ -26,11 +26,22 @@ const reportPost = (id) => {
     showPosts(remainingPosts);
 };
 
+//  const displayContent = (text) => {
+//     return text.length <= 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+// }; 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
-};
+  if(text.length  < 30 ){
+   return text;
+  }
+  else{
+    slicess=text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+     return slicess;
+  }
+  //  slicess=text.slice(0, 30);
+  // return slicess;
+}; 
 
-const switchTab = (id) => {
+const switchTab = (id)  => {
     if (id === "posts") {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
@@ -52,7 +63,7 @@ const switchTab = (id) => {
 
 const createPost = (post) => {
     const image = post.image; 
-    const userImage = post.userImage; 
+    //const displayContent =post.description;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
     div.innerHTML = `
@@ -63,7 +74,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${userImage}" />
+                    <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -108,7 +119,7 @@ const createPost = (post) => {
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
-                      <img src="${userImage}" />
+                      <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
                     </a>
 
                     <span>Liked by
